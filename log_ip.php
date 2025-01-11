02aee20ae806c22e4f995c66c99da48801039f5a
Jkean2012 <Jkean2012@outlook.com>
2025-01-11 14:32:55 -0500

    Delete log_ip.php

--- log_ip.php
+++
@@ -1,4 +0,0 @@
-<?php //IP Grabber
-//Retrieve data from POST request $ip = $_POST['ip']; $device = $_POST['device']; $address = $_POST['address'];
-
-//Print IP, Device, and Address To Log.TXT $fh = fopen('log.txt', 'a'); fwrite($fh, 'IP Address: '."".$ip ."\n"); fwrite($fh, 'Device: '."".$device ."\n"); fwrite($fh, 'Address: '."".$address ."\n\n"); fclose($fh); ?>
